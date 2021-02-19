08f1cff70091d7d0deabb2de439e5cae67dbb9c0
Your Name <you@example.com>
2021-02-19 21:03:15 +0100

    test

--- src/main/java/com/example/rest/Main.java
+++ src/main/java/com/example/rest/Main.java
@@ -1,5 +1,4 @@
 package com.example.rest;
-
 import com.example.rest.models.BibliothequeDaoRepositoryJPA;
 import com.example.rest.models.Livre;
 import org.glassfish.grizzly.http.server.HttpServer;
@@ -7,14 +6,16 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URI;
-
+import java.util.Scanner;
 /**
  * Main class.
  *
  */
 public class Main {
-
     // Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://0.0.0.0:8091/";
     /**
@@ -27,23 +28,23 @@ public class Main {
         final ResourceConfig rc = new ResourceConfig().packages("com.example.rest" , "com.example.rest.services");
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI),rc);
     }
-
     /**
      * Main method.
      * @param args
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
+
 BibliothequeDaoRepositoryJPA bibliothequeDaoRepositoryJPA= new BibliothequeDaoRepositoryJPA();
 
-Livre livre  = new Livre("crypto" , "Stinson") ;
+/*Livre livre  = new Livre("crypto" , "Stinson") ;
 livre.setCategorie("ousm");
         System.out.println(livre.toString());
         bibliothequeDaoRepositoryJPA.save(livre);
-
-      /*  System.out.println(bibliothequeDaoRepositoryJPA.findAll());
+*
+/*  System.out.println(bibliothequeDaoRepositoryJPA.findAll());
         System.out.println( bibliothequeDaoRepositoryJPA.findByCategorie("cat111"));
 */
         //System.out.println("Generated Employee ID = " + employee.getEmployeeId());
@@ -53,7 +54,6 @@ livre.setCategorie("ousm");
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
         System.in.read();
         server.stop();
-
     }
 }
 
